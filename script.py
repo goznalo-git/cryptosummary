76768a7b3ba053e5aca9722920b5cc672b818556
Gonzalo <goznalomail@gmail.com>
2021-04-11 19:28:30 +0200

    script fetching credentials from external file

--- script.py
+++ script.py
@@ -1,16 +1,27 @@
+# Import necessary modules
 import mysql.connector
 import requests
 
+
+# Obtain database credentials
+with open("credentials.txt", "r") as f:
+    creds = f.read().splitlines()
+
+# Connect to MySQL using the module
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="sample_user",
-    password="sample_password",
-    database="cryptocoins"
+    host=creds[0],
+    user=creds[1],
+    password=creds[2],
+    database=creds[3]
 )
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM my_money;")
+query = "SELECT * FROM my_money;"
 
-for line in mycursor:
+mycursor.execute(query)
+
+myresult = mycursor.fetchall()
+
+for line in myresult:
     print(line)
