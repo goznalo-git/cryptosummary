7bb687cab3c0a50611b2744d2ee94ee8065e5bc3
Gonzalo <goznalomail@gmail.com>
2021-04-13 21:24:54 +0200

    Some clarifications

--- script.py
+++ script.py
@@ -40,8 +40,9 @@ def get_portfolio(cols, cursor):
     return portfolio
 
 
+# Database connection.
+# We have to make sure it does connect, otherwise give hints on why not.
 try:
-    # Database connection
     mydb = mysql.connector.connect(
         host=creds[0],
         user=creds[1],
@@ -53,14 +54,14 @@ try:
     portfolio = get_portfolio(*query_database(mydb, query))
 
 except mysql.connector.errors.InterfaceError:
-    print(bold + "MySQL connection Error" + end +
-          ": MySQL is probably not running. Please start it, with\n\tsudo systemctl start mysql\nif you are running a modern Linux system.")
+    print(bold + "MySQL connection Error:" + end +
+          " MySQL is probably not running. Please start it, with\n\tsudo systemctl start mysql\nif you are running a modern Linux system.")
     print("It may also be that the host provided is wrong, be sure it is the appropriate one (usually localhost)\n")
     exit()
 
 except mysql.connector.errors.ProgrammingError:
-    print(bold + "MySQL credentials error" + end +
-          ": The credentials provided (user, password, database) are not correct.\n")
+    print(bold + "MySQL credentials error:" + end +
+          " The credentials provided (user, password, database) are not correct.\n")
     exit()
 
 except:
@@ -74,14 +75,15 @@ def get_crypto_price(crypto=""):
     URL = "https://eur.rate.sx/" + crypto
     response = requests.get(URL)
 
+    # The response has a \n character at the end, we strip it and format the output
     return response.text[:-1]
 
 
+# Loop over each wallet, displaying and storing the values, converted to EUR
 balance = {}
-
 for source in portfolio:
     wallet = portfolio[source]
-    print(source)
+    print(bold + source + end)
 
     balance[source] = 0
 
@@ -99,15 +101,16 @@ for source in portfolio:
                       ": there's no conversion rate available for such cryptocoin, in eur.rate.sx.")
                 continue
 
-            # the response has a \n character at the end, we strip it and format the output
             print("\t" + crypto + " -> " +
                   str(round(amount, 3)) + " €")
     print()
 
-
-[print(source + " total: " + str(round(balance[source], 3)) + " €")
+# Sum all within a wallet
+[print(bold + source + " total: " + end + str(round(balance[source], 3)) + " €")
  for source in balance]
 
 print()
 
-print("Total total: " + str(round(sum(balance.values()), 3)) + " €")
+# Sum all
+print(bold + "Total, total: " + end +
+      str(round(sum(balance.values()), 3)) + " €")
